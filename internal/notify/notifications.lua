072666fed64fa0a8ba6118372e1157c9604d0694
Squid Coder <squid@squidcoder.com>
2025-02-09 17:18:15 -0600

    Fix adv hang

--- internal/notify/notifications.lua
+++ internal/notify/notifications.lua
@@ -324,15 +324,16 @@ local function get_save_alert()
 end
 
 local function save_popup()
-    local mins_since_save = dfhack.persistent.getUnsavedSeconds()//60
-    local message = 'It has been ' .. dfhack.formatInt(mins_since_save) .. ' minutes since your last save. \n\nWould you like to save now? ' ..
-    '(Note: You can also close this reminder and save manually)'
-    local adv_warn_msg = "Unfortunately, quicksave doesn't currently work in Adventure mode, so you will need to save manually.\n\n"..
-    'Close this popup to open the options menu and select "Save and continue playing"'
     if dfhack.world.isFortressMode() then
+        local mins_since_save = dfhack.persistent.getUnsavedSeconds()//60
+        local message = 'It has been ' .. dfhack.formatInt(mins_since_save) .. ' minutes since your last save. \n\nWould you like to save now? ' ..
+        '(Note: You can also close this reminder and save manually)'
         dlg.showYesNoPrompt('Save now?', message, nil, function () dfhack.run_script('quicksave') end)
     else
-        dlg.showMessage("Unable to use quicksave", adv_warn_msg, COLOR_WHITE, function () gui.simulateInput(gui.getDFViewscreen(), 'OPTIONS') end)
+        return --FIXME: HANGS DF
+        -- local adv_warn_msg = "Unfortunately, quicksave doesn't currently work in Adventure mode, so you will need to save manually.\n\n"..
+        -- 'Close this popup to open the options menu and select "Save and continue playing"'
+        -- dlg.showMessage("Unable to use quicksave", adv_warn_msg, COLOR_WHITE, function () gui.simulateInput(gui.getDFViewscreen(), 'OPTIONS') end)
     end
 end
 
